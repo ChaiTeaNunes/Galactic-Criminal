0ac57c15fef0dce83414f46c30f12da70f3b8b66
Chaiyawat Nunes <chaiteanunes@gmail.com>
2016-05-16 22:56:24 -0700

    Speed up; fixes

--- game/game.py
+++ game/game.py
@@ -1,7 +1,14 @@
 import tkinter as tk
 import time
 
-RENDER = 0.0025
+# Constant Definition
+
+RENDER = 0.0005
+SPEED = 2
+WIDTH = 384
+HEIGHT = 576
+
+# Class Definition
 
 class Game(object):
     def __init__(self, title):
@@ -10,10 +17,7 @@ class Game(object):
         self.root.resizable(0, 0)
         self.root.wm_attributes("-topmost", 1)
 
-        self.width = 384
-        self.height = 576
-        
-        self.canvas = tk.Canvas(self.root, width=self.width, height=self.height, bd=0, highlightthickness=0)
+        self.canvas = tk.Canvas(self.root, width=WIDTH, height=HEIGHT, bd=0, highlightthickness=0)
 
         self.canvas.pack()
 
@@ -29,7 +33,7 @@ class Status(object):
         self.game.canvas.bind_all("<KeyPress-space>", self.shoot)
         self.game.canvas.bind_all("<KeyRelease-space>", self.stop)
 
-        self.bg = self.game.canvas.create_rectangle(self.images[0].width() / 2, -1, game.width, self.images[0].height() - 4, fill="grey")
+        self.bg = self.game.canvas.create_rectangle(self.images[0].width() / 2, -1, WIDTH, self.images[0].height() - 4, fill="grey")
         self.image = self.game.canvas.create_image(0, -3, image=self.images[0], anchor="nw")
 
     def shoot(self, event):
@@ -46,36 +50,28 @@ class Background(object):
         self.width = self.bg.width()
         self.height = self.bg.height()
 
-        self.images = [None for x in range(0, (int(game.width / self.width) + 1) * (int(game.height / self.height) + 1))]
-
-        image = 0
+        self.images = []
         
-        for x in range(0, int(game.width / self.width) + 1):
-            for y in range(0, int(game.height / self.height) + 1):
-                self.images[image] = self.game.canvas.create_image(x * self.width, y * self.height, image=self.bg, anchor="nw")
-                image += 1
+        for x in range(0, int(WIDTH / self.width) + 1):
+            for y in range(0, int(HEIGHT / self.height) + 1):
+                self.images.append(self.game.canvas.create_image(x * self.width, y * self.height, image=self.bg, anchor="nw"))
         
         self.speed = speed
 
+    # TODO: optimize
     def update(self):
-        image = 0
-        for x in range(0, int(game.width / self.width) + 1):
-            for y in range(0, int(game.height / self.height) + 1):
-                self.game.canvas.move(self.images[image], 0, self.speed)
-                if self.game.canvas.coords(self.images[image])[1] > game.height:
-                    self.game.canvas.coords(self.images[image], (image % (int(game.width / self.width) + 1)) * self.width, 0 - self.height + 1)
-                image += 1
+        i = 0
+        for x in range(0, int(WIDTH / self.width) + 1):
+            for y in range(0, int(HEIGHT / self.height) + 1):
+                self.game.canvas.move(self.images[i], 0, self.speed)
+                if self.game.canvas.coords(self.images[i])[1] > HEIGHT:
+                    self.game.canvas.coords(self.images[i], x * self.width, 0 - self.height + self.speed)
+                    
+                i += 1
 
-class Entity(object):
+class Spaceship(Game):
     def __init__(self, game):
         self.game = game
-
-    def update(self):
-        raise Exception("All entities must have an update!")
-
-class Spaceship(Entity):
-    def __init__(self, game):
-        Entity.__init__(self, game)
         
         self.images = [tk.PhotoImage(file="ship.png"),
                        tk.PhotoImage(file="ship_l.png"),
@@ -86,7 +82,7 @@ class Spaceship(Entity):
         self.game.canvas.bind_all("<KeyRelease-Left>", self.no_steer)
         self.game.canvas.bind_all("<KeyRelease-Right>", self.no_steer)
 
-        self.image = self.game.canvas.create_image((self.game.width - self.images[0].width()) / 2, self.game.height - 100, image=self.images[0], anchor="nw")
+        self.image = self.game.canvas.create_image((WIDTH - self.images[0].width()) / 2, HEIGHT - 100, image=self.images[0], anchor="nw")
 
         self.velX = 0
 
@@ -103,13 +99,17 @@ class Spaceship(Entity):
         self.velX = 0
 
     def update(self):
-        if self.game.canvas.coords(self.image)[0] + self.velX != 0 and self.game.canvas.coords(self.image)[0] + 64 + self.velX != self.game.width:
+        if self.game.canvas.coords(self.image)[0] + self.velX != 0 and self.game.canvas.coords(self.image)[0] + 64 + self.velX != WIDTH:
             self.game.canvas.move(self.image, self.velX, 0)
 
+# Game Initialization
+
 game = Game("Galactic Criminal")
-background = Background(game, "space.png", 1)
+background = Background(game, "space.png", SPEED)
 spaceship = Spaceship(game)
 status = Status(game)
+
+# Game loop
 
 while 1:
     game.root.update()
